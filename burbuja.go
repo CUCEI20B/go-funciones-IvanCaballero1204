db8f1f67eb44289d869d504ad6c438c3a8f71448
repl.it user <replituser@example.com>
2020-10-22 01:19:39 +0000

    Implementación método burbua

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,34 @@
 package main
 
-func Burbuja(s []int64)  {
+import(
+  "fmt"
+)
 
+func Burbuja(s []int64) []int64 {
+	var temp int64 = 0
+
+	for i := 0; i < len(s) - 1; i++{
+		for j := 0; j < len(s) - 1; j++{
+			if(s[j] > s[j+1]){
+				temp = s[j]
+				s[j] = s[j+1]
+				s[j+1] = temp 
+			}
+		}
+	}
+
+	return s
 }
 
 func main()  {
-	
+	slice := []int64{3, 16, 0, 9, 2, 24, 1}
+	slice2 := []int64{0, 1, 2, 3, 9, 16, 24} 
+	new_slice := Burbuja(slice)
+
+	for i, _ := range slice2{
+		if (new_slice[i] != slice2[i]){
+			fmt.Println("fallaste")
+			break
+		}
+	}
 }
